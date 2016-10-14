41bdc73a04b5a8cd5fd55f99e44ac4c6933c3916
Philippe Hürlimann <p@hurlimann.org>
2016-10-14 14:30:08 +0200

    fixed resource leak error

--- src/org/hurlimann/zuul/Parser.java
+++ src/org/hurlimann/zuul/Parser.java
@@ -43,12 +43,13 @@ public class Parser {
 		inputLine = reader.nextLine();
 
 		// Find up to two words on the line.
-		Scanner tokenizer = new Scanner(inputLine);
-		if (tokenizer.hasNext()) {
-			word1 = tokenizer.next(); // get first word
+		try (Scanner tokenizer = new Scanner(inputLine)) {
 			if (tokenizer.hasNext()) {
-				word2 = tokenizer.next(); // get second word
-				// note: we just ignore the rest of the input line.
+				word1 = tokenizer.next(); // get first word
+				if (tokenizer.hasNext()) {
+					word2 = tokenizer.next(); // get second word
+					// note: we just ignore the rest of the input line.
+				}
 			}
 		}
 
